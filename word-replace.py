3c0c7c722b43127ac700acaa1f2d5bf6231e55a1
Shaunbus <sjbucyk@live.ca>
2021-04-26 10:50:36 -0700

    First working solution replacing Jobname and compname and saving to proper file

--- word-replace.py
+++ word-replace.py
@@ -1,18 +1,30 @@
 from docx import Document
 
-
+#read in Document
 document = Document('ShaunB_CV_Template.docx')
 
-# Dictionary of keys
-dic = {'JOBNAME': 'Test', 'COMPNAME': 'zain'}
+
+def WordReplace(document, dictionary):
+    for p in document.paragraphs:
+        inline = p.runs
+        for i in range(len(inline)):
+            text = inline[i].text
+            if text in dic.keys():
+                text = text.replace(text, dic[text])
+                inline[i].text = text
 
 
-for p in document.paragraphs:
-    inline = p.runs
-    for i in range(len(inline)):
-        text = inline[i].text
-        if text in dic.keys():
-            text = text.replace(text, dic[text])
-            inline[i].text = text
+def GetInput():
+    job = input("Job Name: ")
+    comp = input("Comp Name: ")
+    date = input("Date: ")
+    dic = {'JOBNAME': job, 'COMPNAME': comp, 'CURRENTDATE': date}
+    return dic
 
-document.save('new.docx')
+
+# Read in user Input then replace template words
+dic = GetInput()
+WordReplace(document, dic)
+
+# Saving Document
+document.save('ShaunB_CV_' + dic["JOBNAME"] + '.docx')
